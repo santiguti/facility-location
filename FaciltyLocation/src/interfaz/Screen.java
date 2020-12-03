90d91b36ef8587a2171bd145b89a1d7a8cbac62a
Santi <Santi@DESKTOP-6KR91B0>
2020-12-03 07:07:14 -0300

    Agregue package interfaz y Screen.java con todo setteado para empezar a armar la ventana

---
+++ FaciltyLocation/src/interfaz/Screen.java
@@ -0,0 +1,43 @@
+package interfaz;
+
+import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Screen extends JFrame implements ActionListener {
+	private static final long serialVersionUID = 1L;
+	
+	int width, height;
+	
+	//Hacer todos los news de aca para abajo
+	CardLayout layout = new CardLayout();
+	JPanel panel = new JPanel();
+	
+	public Screen(int width, int height) {
+		panel.setLayout(layout);
+		addButtons();
+		
+		setSize(width, height);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setTitle("Tienda Planner");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		requestFocus();
+	}
+	
+	private void addButtons() {
+		//Setteo layouts a null, les agrego background, hago los adds de los botones a los correspondientes layouts
+	}
+	
+	public void actionPerformed(ActionEvent event) {
+		Object source = event.getSource();
+		//Aca se hacen los if(source == boton) entonces layout.show(panel, "nombreDeOtroPanel")
+	}
+	
+	public static void main(String[] args) {
+		Screen s = new Screen(800, 600);
+	}
+}
