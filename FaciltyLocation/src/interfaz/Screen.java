6efe9553a5fd642b328baec68b0abc0834702a62
Santi <Santi@DESKTOP-6KR91B0>
2020-12-06 15:06:44 -0300

    agregue un table model y un jtable para mostrar el arraylist de clients y centers

--- FaciltyLocation/src/interfaz/Screen.java
+++ FaciltyLocation/src/interfaz/Screen.java
@@ -4,8 +4,18 @@ import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import logic.data.Customer;
+import logic.data.DistributionCenter;
+import logic.data.ListCustomer;
+import logic.data.ListDistributionCenter;
 
 public class Screen extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
@@ -17,11 +27,18 @@ public class Screen extends JFrame implements ActionListener {
 	JPanel panel = new JPanel();
 	JPanel menu = new JPanel();
 	
+	ListCustomer clients = new ListCustomer();
+	ListDistributionCenter centers = new ListDistributionCenter();
+	DefaultTableModel clientModel = new DefaultTableModel(1, 2);
+	DefaultTableModel centersModel = new DefaultTableModel(1, 3);
+	JTable clientsList = new JTable(clientModel);
+	JTable centersList = new JTable(centersModel);
+	
 	public Screen(int width, int height) {
 		panel.setLayout(layout);
 		addButtons();
 		
-		setSize(width, height);
+		setSize(800, 600);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setVisible(true);
@@ -31,13 +48,25 @@ public class Screen extends JFrame implements ActionListener {
 	}
 	
 	private void addButtons() {
-		//Setteo layouts a null, les agrego background, hago los adds de los botones a los correspondientes layouts
+		//Setteo layouts a null, les agrego background
 		menu.setLayout(null);
 		menu.setBackground(Color.GRAY);
 		panel.add(menu, "Menu");
 		
 		getContentPane().add(panel);
 		layout.show(panel, "Menu");
+		
+		//Luego agrego labels, buttons, list a los diferentes layouts y les setteo font, posicion, etc
+		menu.add(clientsList);
+		clientsList.setBounds(10, 20, 245, 445);
+		clientModel.setValueAt("Latitude", 0, 0);
+		clientModel.setValueAt("Longitude", 0, 1);
+		
+		menu.add(centersList);
+		centersList.setBounds(539, 20, 245, 445);
+		centersModel.setValueAt("Center ID", 0, 0);
+		centersModel.setValueAt("Latitude", 0, 1);
+		centersModel.setValueAt("Longitude", 0, 2);
 	}
 	
 	public void actionPerformed(ActionEvent event) {
