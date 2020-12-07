9b4f0ab20dc572bc85105676bbc8cea55d9686db
Santi <Santi@DESKTOP-6KR91B0>
2020-12-07 01:43:22 -0300

    marcadores andando

--- FaciltyLocation/src/interfaz/Screen.java
+++ FaciltyLocation/src/interfaz/Screen.java
@@ -4,25 +4,25 @@ import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
-import javax.swing.DefaultListModel;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JList;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JSpinner;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+import org.openstreetmap.gui.jmapviewer.JMapViewer;
+import org.openstreetmap.gui.jmapviewer.Layer;
+import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
+import org.openstreetmap.gui.jmapviewer.interfaces.MapMarker;
 
 import logic.data.Customer;
 import logic.data.DistributionCenter;
 import logic.data.ListCustomer;
 import logic.data.ListDistributionCenter;
-import javax.swing.JTextField;
-import javax.swing.SpinnerNumberModel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
-public class Screen extends JFrame implements ActionListener {
+public class Screen extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	int width, height;
@@ -31,30 +31,15 @@ public class Screen extends JFrame implements ActionListener {
 	CardLayout layout = new CardLayout();
 	JPanel panel = new JPanel();
 	JPanel menu = new JPanel();
-	JPanel map = new JPanel();
 	
-	ListCustomer clients = new ListCustomer();
+	JMapViewer map = new JMapViewer();
+	Coordinate coordinate = new Coordinate(-34.518164, -58.659785);
+	
+	ListCustomer customers = new ListCustomer();
 	ListDistributionCenter centers = new ListDistributionCenter();
-	DefaultTableModel clientModel = new DefaultTableModel(1, 2);
-	DefaultTableModel centersModel = new DefaultTableModel(1, 3);
-	JTable clientsList = new JTable(clientModel);
-	JTable centersList = new JTable(centersModel);
-	
-	JButton addClient = new JButton("Add");
-	JButton removeClient = new JButton("Remove");
-	JSpinner toBeRemovedClient = new JSpinner();
-	JTextField latitudeClient = new JTextField();
-	JTextField longitudeClient = new JTextField();
-	
-	JButton addCenter = new JButton("Add");
-	JButton removeCenter = new JButton("Remove");
-	JSpinner toBeRemovedCenter = new JSpinner();
-	JTextField centerID = new JTextField();
-	JTextField latitudeCenter = new JTextField();
-	JTextField longitudeCenter = new JTextField();
-	
-	JButton menuToMap = new JButton("\u2192");
-	JButton mapToMenu = new JButton("\u2190");
+	JMenuBar menuBar = new JMenuBar();
+	JMenu menuList = new JMenu("Acciones");
+	JMenuItem loadData = new JMenuItem("Cargar datos");
 	
 	public Screen(int width, int height) {
 		panel.setLayout(layout);
@@ -74,89 +59,45 @@ public class Screen extends JFrame implements ActionListener {
 		menu.setLayout(null);
 		menu.setBackground(Color.GRAY);
 		panel.add(menu, "Menu");
-		
-		map.setLayout(null);
-		map.setBackground(Color.black);
-		panel.add(map, "Map");
-		
+
 		getContentPane().add(panel);
 		layout.show(panel, "Menu");
-		
 		//Luego agrego labels, buttons, list a los diferentes layouts y les setteo font, posicion, etc
-		menu.add(clientsList);
-		clientsList.setBounds(10, 59, 500, 260);
-		clientModel.setValueAt("Latitude", 0, 0);
-		clientModel.setValueAt("Longitude", 0, 1);
+		menu.add(map);
+		map.setBounds(0, 29, 1008, 698);
+		map.setDisplayPosition(coordinate, 12);
+		map.setZoomControlsVisible(false);
 		
-		menu.add(addClient);
-		addClient.setBounds(680, 171, 150, 25);
+		menu.add(menuBar);
+		menuBar.setBounds(0, 0, 1018, 30);
+		menuBar.add(menuList);
 		
-		menu.add(removeClient);
-		removeClient.setBounds(520, 280, 150, 25);
-		
-		menu.add(toBeRemovedClient);
-		toBeRemovedClient.setBounds(680, 280, 40, 25);
-		toBeRemovedClient.setModel(new SpinnerNumberModel(0, 0, clients.size(), 1));
-		
-		menu.add(latitudeClient);
-		latitudeClient.setBounds(596, 123, 150, 25);
-		menu.add(longitudeClient);
-		longitudeClient.setBounds(756, 123, 150, 25);
-		
-		menu.add(centersList);
-		centersList.setBounds(10, 409, 500, 260);
-		centersModel.setValueAt("Center ID", 0, 0);
-		centersModel.setValueAt("Latitude", 0, 1);
-		centersModel.setValueAt("Longitude", 0, 2);
-		
-		menu.add(addCenter);
-		addCenter.setBounds(680, 494, 150, 25);
-		
-		menu.add(removeCenter);
-		removeCenter.setBounds(520, 610, 150, 25);
-		
-		menu.add(toBeRemovedCenter);
-		toBeRemovedCenter.setBounds(680, 610, 40, 25);
-		toBeRemovedCenter.setModel(new SpinnerNumberModel(0, 0, centers.size(), 1));
-		
-		menu.add(centerID);
-		centerID.setBounds(520, 446, 150, 25);
-		menu.add(latitudeCenter);
-		latitudeCenter.setBounds(680, 446, 150, 25);
-		menu.add(longitudeCenter);
-		longitudeCenter.setBounds(840, 446, 150, 25);
-		
-		menu.add(menuToMap);
-		menuToMap.setBounds(880, 675, 95, 35);
-		
-		map.add(mapToMenu);
-		mapToMenu.setBounds(10, 10, 50, 20);
-	}
-	
-	public void actionPerformed(ActionEvent event) {
-		Object source = event.getSource();
-		//Aca se hacen los if(source == boton) entonces layout.show(panel, "nombreDeOtroPanel")
-		if(source == menuToMap)
-			layout.show(panel, "Map");
-		else if (source == mapToMenu)
-			layout.show(panel, "Menu");
-		else if (source == addClient) {
-			if(latitudeClient.getText().isEmpty() || longitudeClient.getText().isEmpty())
-				JOptionPane.showMessageDialog(null, "Verifique que la latitud/longitud del cliente sea correcta");
-			else {
-				clients.addCustomer(Double.parseDouble(latitudeClient.getText()), Double.parseDouble(longitudeClient.getText()));
+		menuList.setMnemonic(KeyEvent.VK_A);
+		menuList.add(loadData);
+		loadData.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				customers = ListCustomer.readJSON("ListCustomer.JSON");
+				centers = ListDistributionCenter.readJSON("ListDistributionCenters.JSON");
+				int contador = 1;
+				for (Customer customer : customers.getCustomers()) {
+					MapMarker center1 = new MapMarkerDot("Cliente " + contador, new Coordinate(customer.getLatitude(), customer.getLongitude()));
+					center1.getStyle().setBackColor(Color.blue);
+					center1.getStyle().setColor(Color.black);
+					map.addMapMarker(center1);
+					contador++;
+				}
+				contador = 1;
+				for (DistributionCenter centers : centers.getCenters()) {
+					MapMarker center1 = new MapMarkerDot("Local " + contador, new Coordinate(centers.getLatitude(), centers.getLongitude()));
+					center1.getStyle().setBackColor(Color.red);
+					center1.getStyle().setColor(Color.yellow);
+					map.addMapMarker(center1);
+					contador++;
+					}
 			}
-			
-		}
-		else if (source == addCenter) {
-			if (centerID.getText().isEmpty() ||latitudeCenter.getText().isEmpty() || longitudeCenter.getText().isEmpty())
-				JOptionPane.showMessageDialog(null, "Verifique que el ID o latitud/longitud del local sea correcta");
-			else {
-				centers.addDistributionCenter(centerID.getText(), Double.parseDouble(latitudeCenter.getText()), Double.parseDouble(longitudeCenter.getText()));
-			}
-		}
+		});
 	}
-	
+		
 	public static void main(String[] args) {
 		Screen s = new Screen(800, 600);
 	}
